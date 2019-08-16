5cf3979030feec9e29b99c753c760dc283d9f9df
akhanpaybyphone <53565984+akhanpaybyphone@users.noreply.github.com>
2019-08-15 19:15:12 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-# Obtain the component repository and log in
+## Obtain the application repository and log in
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm  \
     -e KBC_DEVELOPERPORTAL_USERNAME \
     -e KBC_DEVELOPERPORTAL_PASSWORD \
     quay.io/keboola/developer-portal-cli-v2:latest \
     ecr:get-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP}`
-
 eval $(docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME \
     -e KBC_DEVELOPERPORTAL_PASSWORD \
@@ -21,9 +20,10 @@ docker tag ${APP_IMAGE}:latest ${REPOSITORY}:latest
 docker push ${REPOSITORY}:${TRAVIS_TAG}
 docker push ${REPOSITORY}:latest
 
-# Update the tag in Keboola Developer Portal -> Deploy to KBC
-if echo ${TRAVIS_TAG} | grep -c '^v\?[0-9]\+\.[0-9]\+\.[0-9]\+$'
+
+if echo ${TRAVIS_TAG} | grep -c '^[0-9]\+\.[0-9]\+\.[0-9]\+$'
 then
+    # Deploy to KBC -> update the tag in Keboola Developer Portal (needs $KBC_DEVELOPERPORTAL_VENDOR & $KBC_DEVELOPERPORTAL_APP)
     docker run --rm \
         -e KBC_DEVELOPERPORTAL_USERNAME \
         -e KBC_DEVELOPERPORTAL_PASSWORD \
